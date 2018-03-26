d6f2092df7b34bca4d87e5c99d6afed11a61d692
Rajat sharma <rajatsharma369007@gmail.com>
2018-03-26 09:38:40 +0530

    Update Bresenham_line.C

--- bresenham_line/Bresenham_line.C
+++ bresenham_line/Bresenham_line.C
@@ -11,10 +11,13 @@ int main()
 	printf("give ends points\n");
 	scanf("%d %d",&x1,&y1);
 
+	// for initializing the graphic driver
 	initgraph(&gd,&gm,"C:\\turboc3\\bgi");
+	
 	x1-=x0;
 	y1-=y0;
-	p0=2*y1-x1;
+	p0=2*y1-x1;	// decision parameter
+	
 	printf("\tdecision parameter%d ",p0);
 	putpixel(x0,y0,10);
 	for(k=0;k<x1;k++)
