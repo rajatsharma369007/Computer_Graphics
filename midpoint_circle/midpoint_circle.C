982e1bb4299361fd1fc333f5e42d88f53669628e
Rajat sharma <rajatsharma369007@gmail.com>
2018-03-27 23:22:09 +0530

    Add files via upload

---
+++ midpoint_circle/midpoint_circle.C
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<graphics.h>
+ 
+void drawcircle(int x0, int y0, int radius)
+{
+    int x = radius;
+    int y = 0;
+    int p = 0;
+
+    while (x >= y)
+    {
+    putpixel(x0 + x, y0 + y, 7);
+    delay(10);
+    putpixel(x0 + y, y0 + x, 7);
+    delay(10);
+    putpixel(x0 - y, y0 + x, 7);
+    delay(10);
+    putpixel(x0 - x, y0 + y, 7);
+    delay(10);
+    putpixel(x0 - x, y0 - y, 7);
+    delay(10);
+    putpixel(x0 - y, y0 - x, 7);
+    delay(10);
+    putpixel(x0 + y, y0 - x, 7);
+    delay(10);
+    putpixel(x0 + x, y0 - y, 7);
+    delay(10);
+    if (p <= 0)
+    {
+		y += 1;
+		p += 2*y + 1;
+    }
+
+    if (p > 0)
+    {
+		x -= 1;
+		p -= 2*x + 1;
+    }
+    }
+}
+
+int main()
+{
+    int gdriver=DETECT, gmode, x, y, r;
+    initgraph(&gdriver, &gmode, "c:\\turboc3\\bgi");
+
+    printf("Enter radius of circle: ");
+    scanf("%d", &r);
+
+    printf("Enter co-ordinates of center(x and y): ");
+    scanf("%d%d", &x, &y);
+    drawcircle(x, y, r);
+    getch();
+    closegraph();
+    return 0;
+}
